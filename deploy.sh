bda359c2b7deeb051095ec6a5cb65aca9098d434
Logan <git@loganwhite.org>
2022-07-30 03:51:44 +0100

    Deploy script for gh-pages

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+git init
+git add -A
+git commit -m 'Deploy'
+git push -f git@github.com:george-hm/pokemon-matchup.git master:gh-pages
+cd -
