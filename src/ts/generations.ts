5c2430d88c1ebb6a38a23e2615a7396702d80421
George <georgehm@pm.me>
2022-07-31 00:54:36 +0100

    Break down api into separate files

---
+++ src/ts/generations.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+export enum Generations {
+    GEN_LATEST = 'latest',
+    GEN_1 = 1,
+    GEN_2 = 2,
+    GEN_3 = 3,
+    GEN_4 = 4,
+    GEN_5 = 5,
+    GEN_6 = 6,
+    GEN_7 = 7,
+    GEN_8 = 8,
+}
