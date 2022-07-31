7974f4078f12f8153a1adcb05afaa02ce9b09fd0
Logan <git@loganwhite.org>
2022-07-31 05:48:13 +0100

    Update spacing

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/pokemon-matchup/' : '/',
+    transpileDependencies: true,
+    lintOnSave: false,
+    publicPath: process.env.NODE_ENV === 'production' ? '/pokemon-matchup/' : '/',
 });
