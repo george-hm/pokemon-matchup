2b2b56a9d5f9192f137b323d0d01190b8cb65111
Logan <git@loganwhite.org>
2022-07-31 05:48:43 +0100

    Import naive-ui components

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,37 @@
 import { createApp } from 'vue';
+import {
+    create,
+    NDivider,
+    NH4,
+    NImage,
+    NInputNumber,
+    NGrid,
+    NGridItem,
+    NRadioButton,
+    NRadioGroup,
+    NSelect,
+    NSpace,
+    NSpin,
+    NTable,
+} from 'naive-ui';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const naive = create({
+    components: [
+        NDivider,
+        NH4,
+        NImage,
+        NInputNumber,
+        NGrid,
+        NGridItem,
+        NRadioButton,
+        NRadioGroup,
+        NSelect,
+        NSpace,
+        NSpin,
+        NTable,
+    ],
+});
+createApp(App)
+    .use(naive)
+    .mount('#app');
