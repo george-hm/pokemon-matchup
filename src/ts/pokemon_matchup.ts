fcfdd94d498d85813428797215415191ddca5f2a
George <georgehm@pm.me>
2022-08-01 16:38:58 +0100

    Pass gen to matchup

--- src/ts/pokemon_matchup.ts
+++ src/ts/pokemon_matchup.ts
@@ -1,21 +1,35 @@
+import { Generations } from './generations';
 import { Pokemon } from './pokemon';
-import { Type } from './types';
+import { getMatchupForGeneration, getTypesForGeneration } from './types';
 
 export interface PokemonMatchup {
     to: { [type: string]: number};
     from: { [type: string]: number};
 }
 
-export function getPokemonMatchup(pokemonYou: Pokemon, pokemonOpponent: Pokemon): PokemonMatchup {
-    const yourTypes = pokemonYou.typeVersions.latest; // @TODO use generation here
-    const opponentTypeNames = pokemonOpponent.typeVersions.latest.map((type: Type) => type.name);
+export function getPokemonMatchup(
+    pokemonYou: Pokemon,
+    pokemonOpponent: Pokemon,
+    generation: Generations,
+): PokemonMatchup {
+    const yourTypes = getTypesForGeneration(
+        pokemonYou.typeVersions,
+        generation,
+    );
+    const opponentTypeNames = getTypesForGeneration(
+        pokemonOpponent.typeVersions,
+        generation,
+    ).map((type) => type.name);
     const pM: PokemonMatchup = {
         to: {},
         from: {},
     };
     for (let ii = 0; ii < yourTypes.length; ii += 1) {
         const yourType = yourTypes[ii].name;
-        const matchups = yourTypes[ii].versions.latest; // @TODO use generation here
+        const matchups = getMatchupForGeneration(
+            yourTypes[ii],
+            generation,
+        );
         for (let jj = 0; jj < opponentTypeNames.length; jj += 1) {
             const opponentType = opponentTypeNames[jj];
             if (matchups.doubleDamageTo.includes(opponentType)) {
