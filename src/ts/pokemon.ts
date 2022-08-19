865931041cee083cc9dfc6718298457e646a5bce
George <georgehm@pm.me>
2022-08-19 17:54:09 +0100

    Load and set type info

--- src/ts/pokemon.ts
+++ src/ts/pokemon.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { SelectOption } from './component_interfaces';
-import { buildFromRawTypes, Type, Versions } from './types';
+import { convertGenStringToEnum, Generations } from './generations';
+import { getTypeVersionsFromName, MinimalTypeInfo, Versions } from './types';
 
 const BASE_URL = 'https://pokeapi.co/api/v2/';
 
 export interface Pokemon {
     name: string;
-    typeVersions: Versions<Type[]>;
+    typeVersions: Versions<MinimalTypeInfo[]>;
     id: string;
     imageUrl: string;
 }
@@ -22,7 +23,44 @@ export async function getPokemonById(id: string|number): Promise<Pokemon> {
 
     const data = rawPokemonData?.data;
 
-    const typeVersions = await buildFromRawTypes(data);
+    const typeVersions: Versions<MinimalTypeInfo[]> = {
+        [Generations.GEN_LATEST]: [],
+    };
+
+    let allTypeNames: string[] = [];
+    for (let i = 0; i < data.types.length; i += 1) {
+        const currentType = data.types[i];
+        allTypeNames.push(currentType.type.name);
+        typeVersions[Generations.GEN_LATEST].push({
+            name: currentType.type.name,
+            primary: currentType.slot === 1,
+        });
+    }
+
+    if (data?.past_types) {
+        for (let i = 0; i < data.past_types.length; i += 1) {
+            const versionInfo = data.past_types[i];
+            const version = convertGenStringToEnum(versionInfo.generation.url);
+            if (!typeVersions[version]) {
+                typeVersions[version] = [];
+            }
+
+            // now loop through the types
+            for (let x = 0; x < versionInfo.types.length; x += 1) {
+                const currentType = versionInfo.types[x];
+                allTypeNames.push(currentType.type.name);
+                typeVersions[version]?.push({
+                    name: currentType.type.name,
+                    primary: currentType.slot === 1,
+                });
+            }
+        }
+    }
+
+    // dedupe all type names
+    allTypeNames = [...new Set(allTypeNames)];
+    // load all the types fully to put them into cache
+    await Promise.all(allTypeNames.map(getTypeVersionsFromName));
 
     const pokemon: Pokemon = {
         name: data.name,
